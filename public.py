55039aece40100a81e63ba6c75df0a020def0638
Hokiradji <iva.blagov@gmail.com>
2021-12-05 20:12:10 +0300

    RR

---
+++ public.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("git add .")
+a = input("Введите название коммита")
+os.system(f'git commit -m {a}')
+os.system("git push origin source")
+os.system("bundle exec rake generate")
+os.system("bundle exec rake deploy")
